8850e62b8a4818d25b2417cab9d475e9ebc84936
Luna Kang <luna.kang.ca@gmail.com>
2017-03-21 15:29:33 -0700

    Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :videos, only: [:new, :index]
   root to: 'videos#index'
+
+  get '/auth/:provider/callback', to: 'sessions#create'
+  get '/auth/failure', to: 'sessions#fail'
 end
